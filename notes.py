fa9eeefd1e6dbdba4078a55307b6e0734fb4f2ef
Salim Djerbouh <tbitw31@gmail.com>
2017-01-02 02:55:46 +0100

    Multiple division

---
+++ notes.py
@@ -0,0 +1,7 @@
+# the double slashes // does a simple division and then round down the result
+# which means // = / + floor(result)
+# Example below
+print(18/4) # print out 4.5
+print(18//4) # print out 4
+x = 18 /4
+print(round(x)) # print out 4

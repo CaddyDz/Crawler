4f151243aec4987b43e5e945ce66574d7b78679c
Salim Djerbouh <tbitw31@gmail.com>
2017-01-05 19:47:29 +0100

    test

---
+++ all/import.py
@@ -0,0 +1,14 @@
+import random
+import sys
+import os
+
+print("Hello World")
+
+# A Single line comment
+
+''' Multi
+Line
+Comment '''
+
+# Store variables
+name = "Salim"
---
+++ if.py
@@ -0,0 +1,8 @@
+# Opposed to C like syntax languages! Python uses the : instead of ()
+age = 15
+if age > 18:
+    print("You are allowed to drive")
+elif age > 60:
+    print("You are too old to drive")
+else:
+    print("You are too young to drive")

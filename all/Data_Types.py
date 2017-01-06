02a43b4964dd57b192d53a8e4693cc1eb0ed9822
Salim Djerbouh <tbitw31@gmail.com>
2017-01-06 23:18:54 +0100

    test

---
+++ all/Data_Types.py
@@ -0,0 +1,6 @@
+# The data types available on python are Numbers, Strings, Lists, Tuples and Dictionaries
+
+number = 5
+string = "Salim"
+List = ['Numbers', 'Strings', 'Lists', 'Tuples', 'Dictionaries']
+Tupes = 

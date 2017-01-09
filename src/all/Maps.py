ec2e65f7d02bdfa7803797037223d6a585a78992
Salim Djerbouh <tbitw31@gmail.com>
2017-01-09 21:47:49 +0100

    Learned Dictionaries :tada:

--- src/all/Maps.py
+++ src/all/Maps.py
@@ -14,4 +14,11 @@ del super_villains['Fiddler']
 print(super_villains)
 # Values of keys can be updated in place 
 super_villains['Pied Piper'] = 'Hartley Rathaway'
-
+# Get the length of the dictionary
+print(len(super_villains))
+# Note that both keys and values can be extracted
+print(super_villains.get("Pied Piper"))
+# Get the list of keys in the dictionary
+print(super_villains.keys())
+# Get the list of values in the dictionary
+print(super_villains.values())

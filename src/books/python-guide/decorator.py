b49ba1693a06f6d7dbc6b881b4ce791a38060464
Caddy Dz <tbitw31@gmail.com>
2017-01-11 23:49:07 +0100

    Haha :tada:

---
+++ src/books/python-guide/decorator.py
@@ -0,0 +1,5 @@
+# A decorator is a simple yet powerful syntax provided by Python
+# A decorator is simply a function or a class that wraps (or decorates) a function or a method.
+# The 'decorated' function or method will replace the original one 'undecorated'
+# because functions are first class objects in Python.
+# Example

428ba8f82b372a9b3e89469fa9cb5f0644717f89
Salim Djerbouh <tbitw31@gmail.com>
2017-01-12 18:05:13 +0100

    Learned about decorators   :gift_heart: Basically docerator allows functions wrapping within other functions. - Test for a decoration with a sample foo and bar functions.

--- src/books/python-guide/decorator.py
+++ src/books/python-guide/decorator.py
@@ -3,3 +3,21 @@
 # The 'decorated' function or method will replace the original one 'undecorated'
 # because functions are first class objects in Python.
 # Example
+
+
+def foo():
+    5 + 5
+
+
+def decorator(func):
+    # Manipulate func
+    return func
+
+foo = decorator(foo)  # Manually decorate
+
+
+@decorator
+def bar():
+    5 + 5
+    # Do something
+    # bar() is decorated

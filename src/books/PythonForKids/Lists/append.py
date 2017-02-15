426fb3b710d7f3a27bd794a0bafa0c14af4de52a
Salim Djerbouh <tbitw31@gmail.com>
2017-02-15 23:37:11 +0100

    Appending items to a list

--- src/books/PythonForKids/Lists/append.py
+++ src/books/PythonForKids/Lists/append.py
@@ -1,2 +1,7 @@
-import __ini
+wizard_list = ['spider legs', 'toe of frog', 'eye of newt', 'bat wing', 'slug butter', ]
+wizard_list.append('bear burp')
 print(wizard_list)
+wizard_list.append('mandrake')
+wizard_list.append('hemlock')
+wizard_list.append('swamp gas')
+print (wizard_list)

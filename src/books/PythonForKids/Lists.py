7424d483f5e7ddd6f3cdee3a2db9bb2e1e0d4d9a
Salim Djerbouh <tbitw31@gmail.com>
2017-02-15 22:46:35 +0100

    Lists

---
+++ src/books/PythonForKids/Lists.py
@@ -0,0 +1,7 @@
+wizard_list = ['spider legs', 'toe of frog', 'eye of newt', 'bat wing', 'slug butter', 'snake dandruff']
+wizard_list[2] = 'snail tongue'
+print(wizard_list[2:5])
+some_numbers = [1, 2, 5, 10, 20]
+some_strings = ['Which', 'Witch', 'Is', 'Which']
+numbers_and_strings = ['Why', 'was', 6, 'afraid', 'of', 7, 'because', 7, 8, 9]
+print(numbers_and_strings)

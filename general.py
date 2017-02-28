2d1d367b88fe4848a4296cc99cb4a076b68bac34
Salim Djerbouh <tbitw31@gmail.com>
2017-02-28 16:33:45 +0100

    FS config

--- general.py
+++ general.py
@@ -2,28 +2,52 @@ import os
 
 
 # Each website you crawl is a separate project (folder)
-def createProjectDir(directory):
+
+
+def create_project_dir(directory):
     if not os.path.exists(directory):
         print('Creating project ' + directory)
         os.makedirs(directory)
 
 
 # Create queue and crawled files (if not crawled)
-def createDataFiles(project_name, base_url):
+
+
+def create_data_files(project_name, base_url):
     queue = project_name + '/queue.txt'
     crawled = project_name + '/crawled.txt'
     if not os.path.isfile(queue):
-        writeFile(queue, base_url)
+        write_file(queue, base_url)
     if not os.path.isfile(crawled):
-        writeFile(crawled, '')
+        write_file(crawled, '')
 
 
 # Create a new file
-def writeFile(path, data):
+
+
+def write_file(path, data):
     f = open(path, 'w')
     f.write(data)
     f.close()
 
-createDataFiles('caddy', 'https://caddydz.github.io/')
+# Add data onto an existing file
+
+
+def append_to_file(path, data):
+    with open(path, 'a') as _file:
+        _file.write(data + '\n')
+
+# Delete the content of a file
+
+
+def delete_file_content(path):
+    with open(path, 'w'):
+        pass
+
+
+
+
+
+
 
 

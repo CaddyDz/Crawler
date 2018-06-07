b3cfaea82c968e53506965690078bdb95d8caab4
Salim Djerbouh <github@caddydz.me>
2018-06-07 14:30:58 +0100

    Using spider arguments
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -2,22 +2,20 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
-    start_urls = [
-        'http://quotes.toscrape.com/page/1/',
-    ]
+
+    def start_requests(self):
+        url = 'http://quotes.toscrape.com/'
+        tag = getattr(self, 'tag', None)
+        if tag is not None:
+            url = url + 'tag/' + tag
+        yield scrapy.Request(url, self.parse)
+
     def parse(self, response):
         for quote in response.css('div.quote'):
             yield {
                 'text': quote.css('span.text::text').extract_first(),
-                'author': quote.css('small.author::text').extract_first(),
-                'tags': quote.css('div.tags a.tag::text').extract(),
+                'author': quote.css('small.author::text').extract_first()
             }
         next_page = response.css('li.next a::attr(href)').extract_first()
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
-
-        for href in response.css('li.next a::attr(href)'):
-            yield response.follow(href, callback=self.parse)
-
-        for a in response.css('li.next a'):
-            yield response.follow(a, callback=self.parse)
